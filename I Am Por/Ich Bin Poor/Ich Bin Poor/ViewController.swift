57c6427b759c0011908ca35d3710a01fcf2dd079
Hudson <hdoerr@vols.utk.edu>
2020-07-05 12:56:00 -0400

    Uploading IamRich and IamPoor

---
+++ I Am Por/Ich Bin Poor/Ich Bin Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ich Bin Poor
+//
+//  Created by Hudson Doerr on 7/4/20.
+//  Copyright © 2020 Hudson Doerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IamRich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Hudson Doerr on 7/3/20.
+//  Copyright © 2020 Hudson Doerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
